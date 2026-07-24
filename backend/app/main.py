73b8386e1e5157301e9266eebd25df1b108fa12d
ruchikk-org <2022mt70182@wilp.bits-pilani.ac.in>
2026-07-24 12:28:36 +0530

    added the updated http

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5175",
         "http://localhost:5173",
-        "http://localhost:5174"
+        "http://localhost:5174",
+        "http://localhost:4173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
